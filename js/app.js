2b7e3fbab5a5bda04b97a59c19eedfa5856500a5
sbhalla <shivam@brigademarketing.com>
2015-05-01 18:18:23 -0400

    bugs

--- js/app.js
+++ js/app.js
@@ -1,3 +1,96 @@
+$(function() {
+    if($('body').hasClass('js')) {
+
+        // ANIMATIONS
+        $('.logo object').addClass('scaleInOut').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+            $(this).animate({
+                left: 0,
+                marginLeft: 0},
+                400, function() {
+                 $(this).next().addClass('fadeInDown').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+                    $(this).css({
+                        opacity : 1
+                    });
+                      $('form').addClass('fadeInUp').css({opacity : 1});
+                 });
+            });
+        });
+        // $('.logo object').addClass('bounceIn animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+        //     $(this).animate({
+        //         left: 0,
+        //         marginLeft: 0},
+        //         400, function() {
+        //          $(this).next().addClass('fadeInDown animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+        //             $('form').addClass('flipInX animated').css({opacity : 1});
+        //          });
+        //     });
+        // });
+
+        $('#search').focus();
+
+        function searchGit() {
+            //grab value of search field
+            var search = $('#search').val();
+
+            // Validates entered value
+            if (search.length > 0) {
+                $(this).find('.error').hide();
+                $('html, body').animate({
+                    scrollTop: 0
+                }, 'fast');
+
+                //pull json data from url
+                $.ajax({
+                    url: 'https://api.github.com/legacy/repos/search/' + search,
+                    dataType: 'json',
+                    cache: true,
+                    success: function(data) {
+                        $('body').removeClass('pre').addClass('post');
+                        // use the results to template the html using _
+                        var results = data.repositories,
+                            resultsTemplate = _.template($("#results-template").html()),
+                            resultingHtml = resultsTemplate({
+                                results : results,
+                                searchVal : search,
+                                amount: results.length
+                            });
+
+                        var overlayTemplate = _.template($("#overlay-template").html()),
+                            overlayHtml = overlayTemplate({
+                                results : results
+                            });
+                        
+                        // place the generated data into the html
+                        $("#overlay-container").html(overlayHtml);
+
+
+
+                        $('.viewDeatails').on('click', function(e) {
+                            var id = $(this).attr('href');
+
+                            for(var i = 0; i < results.length; i++) {
+                                if(results[i].created == id) {
+                                    console.log(results[i]);
+                                }
+                            }
+                            e.preventDefault();
+                        });
+                    }
+                });
+            } else {
+                $(this).find('.error').fadeIn();
+            }
+
+            return false;
+        }
+
+        $('#searchForm').on('submit', searchGit);
+        
+        
+    }
+    
+});
+
 /*
     # Endpoint URL #
     
@@ -35,36 +128,3 @@
       }
     }
 */
-
-    $(function() {
-        function searchGit() {
-            //grab value of search field
-            var search = $('#search').val();
-
-            // Validates entered value
-            if (search.length > 0) {
-                //pull json data from url
-                $.ajax({
-                    url: 'https://api.github.com/legacy/repos/search/' + search,
-                    dataType: 'json',
-                    cache: true,
-                    success: function(data) {
-                        $('body').removeClass('pre').addClass('post');
-                        // use the results to template the html using _
-                        var results = data.repositories,
-                            resultsTemplate = _.template($("#results-template").html()),
-                            resultingHtml = resultsTemplate({results : results});
-                        
-                        // place the generated data into the html
-                        $("#results-container").html(resultingHtml);
-                    }
-                });
-            } else {
-                alert("Please enter repo name");
-            }
-
-            return false;
-        }
-
-        $('#submit').on('click', searchGit);
-    });
